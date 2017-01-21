1177ca14a8cd4e66f9da6dfd8a73871b8088477d
satnamtoor <singh.satnam02@gmail.com>
2017-01-21 11:57:51 +0530

    First time commit

--- app/src/main/java/satnam/valentinelove/Initial.java
+++ app/src/main/java/satnam/valentinelove/Initial.java
@@ -9,5 +9,9 @@ public class Initial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial);
+
+
+
+        //satnam
     }
 }
