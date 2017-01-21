c2cd4e6eec9fbe9888b95f48f3ffd0600ea114f3
satnamtoor <singh.satnam02@gmail.com>
2017-01-21 11:51:47 +0530

    First time commit

---
+++ app/src/main/java/satnam/valentinelove/Initial.java
@@ -0,0 +1,13 @@
+package satnam.valentinelove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Initial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial);
+    }
+}
